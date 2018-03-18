fd8b7fe59d9f116bcbe22b6108be8a7806cc72de
Marius <marius@twostairs.com>
2018-03-18 01:24:55 +0100

    Replaced reload-nginx.sh with containerpilot.sh

---
+++ containerpilot.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+export PATH=/usr/local/bin:$PATH
+
+consul() {
+    export CONSUL_AGENT_BIND_ADDR=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+    echo "Exported CONSOUL_AGENT_BIND_ADDR to $CONSUL_AGENT_BIND_ADDR ..."
+
+    wait-for-http.sh http://$CONSUL_SERVER:8500
+    run-consul-agent.sh $CONSUL_AGENT_BIND_ADDR $CONSUL_SERVER
+}
+
+onStart() {
+    logDebug "onStart"
+
+    logDebug "Running consul-template ..."
+    if [[ ! -f /etc/nginx/nginx.conf ]]; then
+        consul-template -consul-addr=localhost:8500 -once -template=/etc/nginx.conf.tmpl:/etc/nginx/nginx.conf
+        if [[ $? != 0 ]]; then
+            exit 1
+        fi
+    fi
+}
+
+onChange() {
+    logDebug "onChange"
+
+    consul-template -consul-addr=localhost:8500 -once -template=/etc/nginx.conf.tmpl:/etc/nginx/nginx.conf
+    pkill -SIGHUP nginx
+}
+
+health() {
+    logDebug "health"
+
+    /usr/bin/curl -o /dev/null --fail -s http://127.0.0.1:80/health
+    if [[ $? -ne 0 ]]; then
+        echo "Service monitor endpoint failed"
+        exit 1
+    fi
+}
+
+logDebug() {
+    if [[ "${LOG_LEVEL}" == "DEBUG" ]]; then
+        echo "containerpilot.sh: $*"
+    fi
+}
+
+until
+    cmd=$1
+    if [[ -z "$cmd" ]]; then
+        help
+    fi
+    shift 1
+    $cmd "$@"
+    [ "$?" -ne 127 ]
+do
+    help
+    exit
+done
